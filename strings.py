cf8014310659b4ccfca8d12ca7c1114b750865c7
Julien bidart <jbidart@ncsu.edu>
2020-09-03 17:52:48 +0200

    First python lessons/Seems easy for now

---
+++ strings.py
@@ -0,0 +1,39 @@
+print('chilling')
+
+
+# This program is just a training ground to understand python better
+
+ #variables
+
+example_var = 'oui'
+
+camelCase = 'This is just clearer'
+
+functionExample = print('i\'m a function')
+
+x,y = (3,5)
+
+print(x)
+print(y)
+
+#Loops
+
+condition = 1
+
+#while
+while condition < 10:
+    print(condition)
+    condition += 1 
+
+# while True:
+    # print('welcome to infinity')
+    
+#for
+exampleList = [1,1,5,4,65,1,21,95,50,2]
+
+for x in exampleList:
+    print(x)
+    print('testing indentation')
+
+for x in range(1,11):
+    print(x)   
